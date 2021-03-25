a3242dd667eca243fd4dbbd980de62c0ff5fa197
siegerts <stephen.siegert@gmail.com>
2021-03-25 15:33:21 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { API } from "aws-amplify";
+// import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
+  const [msg, setMsg] = useState("");
+
+  const fetchMessage = async () => {
+    let resp = await API.get("apidf68e9a6", "/items");
+    setMsg(resp);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We now have Auth!</h1>
       </header>
+
+      <div>
+        <h1>{msg}</h1>
+        <button onClick={fetchMessage}>Fetch message</button>
+      </div>
+      {/* <AmplifySignOut /> */}
     </div>
   );
 }
